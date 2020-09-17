5aeef99ad1fb7f6b08c7cedcfe11edd02d9dc67e
Brenley Dueck <brenley@boldcommerce.com>
2020-09-17 13:48:11 -0500

    Pull data from contentful in serverless function

--- functions/install.js
+++ functions/install.js
@@ -1,6 +1,28 @@
-exports.handler = function(event, context, callback) {
+const contentful = require('contentful')
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+})
+
+exports.handler = async function(event, context, callback) {
+  const entries = await client.getEntries({
+    content_type: 'product',
+  })
+
+  const products = entries.items.map(function(entry) {
+    return {
+      title: entry.fields.title,
+      variantTitle: entry.fields.variantTitle,
+      weight: entry.fields.weight,
+      taxable: entry.fields.taxable,
+      image: entry.fields.image,
+      requireShipping: entry.fields.requiresShipping,
+      price: entry.fields.price,
+    }
+  })
+
   callback(null, {
     statusCode: 200,
-    body: 'Do Install',
+    body: JSON.stringify({ products }),
   })
 }
